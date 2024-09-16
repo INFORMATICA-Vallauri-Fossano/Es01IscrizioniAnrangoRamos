dfe5129140df041ca400e8a3156acd20db30fe0b
Anrango Ramos Gionsi <acharig47@gmail.com>
2024-09-16 20:22:31 +0200

    Creazione della classe Persona per gestire il form di un'iscrizione

---
+++ Es01AnrangoRamos/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Es01AnrangoRamos
+{
+    partial class frmIscrizione
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCognome = new System.Windows.Forms.Label();
+            this.txtCognome = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblDataNascita = new System.Windows.Forms.Label();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.btnInserire = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.btnRicercaXanno = new System.Windows.Forms.Button();
+            this.btnRicercaXcognome = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCognome
+            // 
+            this.lblCognome.AutoSize = true;
+            this.lblCognome.Location = new System.Drawing.Point(13, 13);
+            this.lblCognome.Name = "lblCognome";
+            this.lblCognome.Size = new System.Drawing.Size(52, 13);
+            this.lblCognome.TabIndex = 0;
+            this.lblCognome.Text = "Cognome";
+            // 
+            // txtCognome
+            // 
+            this.txtCognome.Location = new System.Drawing.Point(71, 13);
+            this.txtCognome.Name = "txtCognome";
+            this.txtCognome.Size = new System.Drawing.Size(188, 20);
+            this.txtCognome.TabIndex = 1;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(71, 45);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(188, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(13, 45);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblDataNascita
+            // 
+            this.lblDataNascita.AutoSize = true;
+            this.lblDataNascita.Location = new System.Drawing.Point(13, 78);
+            this.lblDataNascita.Name = "lblDataNascita";
+            this.lblDataNascita.Size = new System.Drawing.Size(37, 13);
+            this.lblDataNascita.TabIndex = 4;
+            this.lblDataNascita.Text = "Nato il";
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(71, 78);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(188, 20);
+            this.txtData.TabIndex = 6;
+            // 
+            // btnInserire
+            // 
+            this.btnInserire.Location = new System.Drawing.Point(184, 115);
+            this.btnInserire.Name = "btnInserire";
+            this.btnInserire.Size = new System.Drawing.Size(75, 23);
+            this.btnInserire.TabIndex = 7;
+            this.btnInserire.Text = "INSERIRE";
+            this.btnInserire.UseVisualStyleBackColor = true;
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.AllowUserToResizeColumns = false;
+            this.dgv.AllowUserToResizeRows = false;
+            this.dgv.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(276, 12);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersVisible = false;
+            this.dgv.Size = new System.Drawing.Size(512, 263);
+            this.dgv.TabIndex = 8;
+            // 
+            // btnRicercaXanno
+            // 
+            this.btnRicercaXanno.Location = new System.Drawing.Point(123, 144);
+            this.btnRicercaXanno.Name = "btnRicercaXanno";
+            this.btnRicercaXanno.Size = new System.Drawing.Size(136, 23);
+            this.btnRicercaXanno.TabIndex = 9;
+            this.btnRicercaXanno.Text = "RICERCA PER ANNO";
+            this.btnRicercaXanno.UseVisualStyleBackColor = true;
+            // 
+            // btnRicercaXcognome
+            // 
+            this.btnRicercaXcognome.Location = new System.Drawing.Point(98, 173);
+            this.btnRicercaXcognome.Name = "btnRicercaXcognome";
+            this.btnRicercaXcognome.Size = new System.Drawing.Size(161, 23);
+            this.btnRicercaXcognome.TabIndex = 10;
+            this.btnRicercaXcognome.Text = "RICERCA PER COGNOME";
+            this.btnRicercaXcognome.UseVisualStyleBackColor = true;
+            // 
+            // frmIscrizione
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRicercaXcognome);
+            this.Controls.Add(this.btnRicercaXanno);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.btnInserire);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.lblDataNascita);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txtCognome);
+            this.Controls.Add(this.lblCognome);
+            this.Name = "frmIscrizione";
+            this.Text = "Gestione Iscrizioni al Torneo";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCognome;
+        private System.Windows.Forms.TextBox txtCognome;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblDataNascita;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Button btnInserire;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Button btnRicercaXanno;
+        private System.Windows.Forms.Button btnRicercaXcognome;
+    }
+}
+
---
+++ Es01AnrangoRamos/clsPersona.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es01AnrangoRamos
+{
+    public class clsPersona
+    {
+        //dichiarazione delle caratteristiche   /attributi  /campi della classe
+        //li dichiareremo tutti privati --> data hiding, incapsulamento
+
+        private string cognome;
+        private string nome;
+        private DateTime dataNascita;
+
+        //Per ogni campo privato dichiariamo le property pubbliche per gli accessi in lettura e scrittura
+
+        public string Cognome
+        {
+            get { return cognome; }     //per accesso in lettura
+            set {
+                if (controllaNominativo(value))
+                    cognome = value;
+                else
+                    throw new Exception("Cognome non valido, deve contenere almeno 2 lettere");
+            }    
+        }
+        public string Nome
+        {
+            get { return nome; }     //per accesso in lettura
+            set
+            {
+                if (controllaNominativo(value))
+                    nome = value;
+                else
+                    throw new Exception("Nome non valido, deve contenere almeno 2 lettere");
+            }
+        }
+
+        public string DataNascita
+        {
+            get { return dataNascita.ToString(); }
+            set
+            {
+                try
+                {
+                    dataNascita = Convert.ToDateTime(value);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Data di nascita non valida"+ex.Message);
+                }
+            }
+        }
+
+        //metodi pubblici   ==> richiamabili e visibili anche dall'esterno della classe
+        public int VisEta()
+        {
+            //il metodo non ha parametri perchè usa il campo della classe
+            int anni = 0;
+            //compito:calcolare l'età della persona
+            try
+            {
+            anni = DateTime.Now.Year - dataNascita.Year;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("errore:"+ex.Message);
+            }
+
+            return anni;
+        }
+
+        //metodi privati ==> richiamabili e visibili solo all'interno della classe
+        private bool controllaNominativo(string s)
+        {
+            bool esito = true;
+            //compito: controllare che s contenga almeno due caratteri, solo lettere dell'alfabeto, spazio,'
+            if (s.Length >= 2)
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+
+                }
+            }
+            else esito = false;
+
+            return esito;
+        }
+    }
+}
